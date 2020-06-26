7fc4d82b9c7a475f3e5b00e053b6951dd4f99b55
Sebastian Braun <sebastian@elmnt.de>
2020-06-26 11:02:17 +0200

    bugfixes

--- Dockerfile
+++ Dockerfile
@@ -5,17 +5,23 @@ MAINTAINER Sebastian Braun <sebastian.braun@fh-aachen.de>
 RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y -q \
     gettext-base \
     mariadb-server \
+    vim \
  && apt-get clean \
  && rm -rf /var/lib/apt/lists/*
 
-# COPY mariadb-server.cnf /etc/my.cnf.d/mariadb-server.cnf
+COPY entrypoint.sh /
 
-COPY entrypoint.sh entrypoint.sql /
 RUN \ 
     sed -i 's/bind-address.*//' /etc/mysql/mariadb.conf.d/50-server.cnf && \
+    sed -i -e "s/\(\[mysqld\]\)/\1\nskip-host-cache/g" /etc/mysql/mariadb.conf.d/50-server.cnf && \ 
+    sed -i -e "s/\(\[mysqld\]\)/\1\nskip-name-resolve/g" /etc/mysql/mariadb.conf.d/50-server.cnf && \ 
+    rm -rf /var/lib/mysql/* && \
+    mkdir -p /var/lib/mysql /var/run/mysqld /var/log/mysql && \
+    chown -R mysql:mysql /var/lib/mysql /var/run/mysqld /var/log/mysql && \
     chmod +x /entrypoint.sh
 
 EXPOSE 3306/tcp
 
 VOLUME ["/var/lib/mysql"]
 ENTRYPOINT ["/entrypoint.sh"]
+CMD ["/usr/bin/mysqld_safe"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,39 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
-
-# parameters
-PASSWORD=`cat /run/secrets/mysql_password_root`
-
-if [ ! -d "/run/mysqld" ]; then
-    mkdir -p /run/mysqld
-    chown -R mysql:mysql /run/mysqld
-fi
+set -eo pipefail
 
 if [ -d /var/lib/mysql/mysql ]; then
     echo '[i] MySQL directory already present, skipping creation'
+
 else
     echo "[i] MySQL data directory not found, creating initial DBs"
+
+    if [ ! -f "/run/secrets/mysql_init.sql" ]; then
+        echo "[e] mysql_init.sql not found!"
+        exit 1
+    fi
 
     chown -R mysql:mysql /var/lib/mysql
 
     # init database
     echo '[i] Initializing database'
-    /usr/bin/mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mysql_install_db --auth-root-authentication-method=socket --datadir=/var/lib/mysql --skip-test-db
+
+    # start temporary server
+    echo '[i] wait for temporary server to start'
+    mysqld_safe --datadir='/var/lib/mysql' --skip-networking --socket="/tmp/mysql" &
+
+	for i in {30..0}; do
+		if mysql --socket /tmp/mysql --database mysql -e 'SELECT 1' &> /dev/null; then
+			break
+		fi
+		sleep 1
+	done
+
+	if [ "$i" = 0 ]; then
+		echo "[e] Unable to start server."
+        exit 1;
+	fi
+
+    echo "[i] load data from mysql_init.sql"
+    mysql --socket /tmp/mysql < /run/secrets/mysql_init.sql
+
+    echo '[i] shutdown temporary server'
+    if ! mysqladmin shutdown --socket="/tmp/mysql"; then
+		echo "[e] Unable to shut down server."
+        exit 1;
+	fi
+
     echo '[i] Database initialized'
-
-    # create temp file
-    tfile=`mktemp`
-    if [ ! -f "$tfile" ]; then
-        return 1
-    fi
-
-    # run sql in tempfile
-   /usr/bin/envsubst < entrypoint.sql | /usr/sbin/mysqld --user=mysql --bootstrap --verbose=0 --datadir='/var/lib/mysql'
-    if [ -f "/run/secrets/mysql_init.sql" ]; then
-        echo "[i] load data from mysql_init.sql"
-        /usr/sbin/mysqld --user=mysql --bootstrap --verbose=0 --datadir='/var/lib/mysql' < /run/secrets/mysql_init.sql
-    fi
 fi
 
 echo '[i] start running mysqld'
